b1e871613ad01937f886105aa77d76aa072191e2
Abhie <abhikore5577@gmail.com>
2023-10-20 17:29:40 +0530

    js modified

--- js/hello.js
+++ js/hello.js
@@ -1,2 +1,13 @@
-console.log("welcome to javaScript")
-document.write("welcome to javaScript")
+// console.log("welcome to javaScript")
+// console.info("This is for info")
+// console.error("This is for error")
+// console.warn("This is for warning")
+
+// a=10
+// b=40
+// sum=a+b
+// console.log("sum ="+sum)
+// document.write("<h1>sum =",sum ,"</h1>")
+console.log("Welcome to Javascript World")
+
+// document.write("welcome to javaScript")
