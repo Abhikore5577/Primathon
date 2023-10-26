986f451373a31716e90be4bffe2acaa6b566a286
Abhie <abhikore5577@gmail.com>
2023-10-26 16:54:12 +0530

    Operator Ass Modified

--- js Assignment/OperatorAss.js
+++ js Assignment/OperatorAss.js
@@ -33,3 +33,8 @@ console.log(true && true);
 console.log(false && false);
 console.log(true && false);
 
+console.log(true || false);
+console.log(true || true);
+console.log(false || false);
+
+console.log(!true);
