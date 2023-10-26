c24415e9a1b85277bf069329b886b22619f82d2d
Abhie <abhikore5577@gmail.com>
2023-10-26 15:39:11 +0530

    js files added

---
+++ js Assignment/OperatorAss.js
@@ -0,0 +1,35 @@
+let x=5;
+let y=3;
+
+console.log('x+y=' , x+y)
+
+console.log('x-y=' , x-y);
+
+console.log('x*y=', x*y);
+
+console.log('x/y=', x/y)
+
+console.log('++x=', ++x);
+console.log('x++=', x++);
+console.log('x=' ,x);
+
+console.log('--x=', --x);
+console.log('x--=', x--);
+console.log('x=' ,x);
+
+console.log(2==2);
+console.log(2=="2");
+
+console.log(3!=2);
+console.log('hello'!='Hello');
+
+console.log(2==='2');
+console.log(2===2);
+
+console.log(2!=='2');
+console.log(2!==2);
+
+console.log(true && true);
+console.log(false && false);
+console.log(true && false);
+
---
+++ js Assignment/typeofOperator.js
@@ -0,0 +1,15 @@
+const num=10;
+const str="This is strig value";
+// const num2;
+const bool1=true;
+const myfun=function() {};
+const myobj={};
+const num3=null;
+
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool1);
+console.log(typeof myfun);
+console.log(typeof myobj);
+console.log(typeof num3);
